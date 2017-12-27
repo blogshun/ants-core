7f8a264b9683d07f9726d8d4a83ec05ee5486847
雨中的旅行者 <530774513@qq.com>
2017-12-27 20:13:48 +0800

    新增获取AliOss实例

--- src/main/java/com/ants/core/context/AntsTool.java
+++ src/main/java/com/ants/core/context/AntsTool.java
@@ -169,6 +169,14 @@ public final class AntsTool {
         return getActiveMq(brokenUrl, username, password);
     }
 
+    /**
+     * 获取阿里云Oss实例
+     *
+     * @param url
+     * @param accessKeyId
+     * @param accessKeySecret
+     * @return
+     */
     public static AliOssTpl getAliOss(String url, String accessKeyId, String accessKeySecret) {
         url = Prop.getKeyStrValue(url);
         accessKeyId = Prop.getKeyStrValue(accessKeyId);
@@ -178,7 +186,7 @@ public final class AntsTool {
             endpoint = url.substring(url.indexOf(".") + 1, url.length());
             bucketName = url.substring(url.indexOf("//") + 2, url.indexOf("."));
             Log.debug("endpoint:{} , bucketName:{}", endpoint, bucketName);
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException(String.format("AliOss配置错误 -> %s", e.getMessage()));
         }
         String key = "oss_".concat(GenUtil.md5Str(url, accessKeyId, accessKeySecret));
@@ -189,7 +197,7 @@ public final class AntsTool {
         return new AliOssTpl(client, bucketName, endpoint);
     }
 
-    public static AliOssTpl getAliOss(){
+    public static AliOssTpl getAliOss() {
         String url = Prop.getStr("ants.alioss.url");
         String accessKeyId = Prop.getStr("ants.alioss.access-key-id");
         String accessKeySecret = Prop.getStr("ants.alioss.access-key-secret");
